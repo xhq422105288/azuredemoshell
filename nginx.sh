d4870e41d28d76f00c6085fc33943bb554132a7b
xhq422105288 <huaqingxu@hotmail.com>
2019-11-14 20:28:18 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -10,7 +10,7 @@ os_release=$(cat /etc/os-release | grep ^ID_LIKE | awk -F '=' '{print $2}')
 
 case $os_release in
  debian)
-        apt install nginx -y && systemctl enable nginx
+        apt install nginx -y && systemctl enable nginx && systemctl start nginx
         ;;
 esac
 
