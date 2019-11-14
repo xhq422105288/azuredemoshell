5cd1bbb9ee6565b7e6ad2d184c49a907749cc602
xhq422105288 <huaqingxu@hotmail.com>
2019-11-14 20:26:43 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,24 +5,12 @@
 ### END INIT INFO
 
 
-os_release=$(cat /etc/os-release)
-if [ $os_release =~ "rhel" ];then
-    wget https://nginx.org/packages/rhel/7/x86_64/RPMS/nginx-1.16.1-1.el7.ngx.x86_64.rpm -P /tmp && \
-    yum install /tmp/nginx-1.16.1-1.el7.ngx.x86_64.rpm  -y && \
-    systemctl enable nginx
-fi
+os_release=$(cat /etc/os-release | grep ^ID_LIKE | awk -F '=' '{print $2}')
 
-if [ $os_release =~ "ubuntu" ];then
-    apt install nginx -y && \
-    systemctl enable nginx
-fi
 
-if [ $os_release =~ "sles" ];then
-    zypper install nginx -y && \
-    systemctl enable nginx
-fi
+case $os_release in
+ debian)
+        apt install nginx -y && systemctl enable nginx
+        ;;
+esac
 
-if [ $os_release =~ "debian" ];then
-    apt install nginx -y && \
-    systemctl enable nginx
-fi
