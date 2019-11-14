25eec311411d2ef5ddd78eb40ea6c183abcabdc9
xhq422105288 <huaqingxu@hotmail.com>
2019-11-14 21:16:57 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,20 +1,27 @@
-#!/bin/sh
-
+#!/bin/bash
 ### BEGIN INIT INFO
 # Provides:      nginx
 ### END INIT INFO
 
-
-os_release=$(cat /etc/os-release | grep ^ID_LIKE | awk -F '=' '{print $2}')
-
-
-case $os_release in
- debian)
-        apt install nginx -y && systemctl enable nginx && systemctl start nginx
-        ;;
- "rhel fedora")
-        wget https://nginx.org/packages/rhel/7/x86_64/RPMS/nginx-1.16.1-1.el7.ngx.x86_64.rpm -P /tmp && \
-        yum install /tmp/nginx-1.16.1-1.el7.ngx.x86_64.rpm -y && systemctl enable nginx && systemctl start nginx
-        ;;
+source /etc/os-release
+case $ID in
+debian|ubuntu|devuan)
+    apt install nginx -y && systemctl enable nginx && systemctl start nginx
+    ;;
+centos|fedora|rhel)
+    wget https://nginx.org/packages/rhel/7/x86_64/RPMS/nginx-1.16.1-1.el7.ngx.x86_64.rpm -P /tmp && \
+    yum install /tmp/nginx-1.16.1-1.el7.ngx.x86_64.rpm -y && systemctl enable nginx && systemctl start nginx
+    ;;
+*)
+    exit 1
+    ;;
 esac
+
+
+
+
+
+
+
+
 
