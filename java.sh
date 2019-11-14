5e0c7e0c1acecc53095420e2475965e65747373c
xhq422105288 <huaqingxu@hotmail.com>
2019-11-14 22:02:06 +0800

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:      jdk-8
+### END INIT INFO
 
+source /etc/os-release
+case $ID in
+debian|ubuntu|devuan)
+    apt install openjdk-8-jdk -y
+    ;;
+centos|fedora|rhel)
+    yum install openjdk-8-jdk -y
+    ;;
+*)
+    exit 1
+    ;;
+esac
